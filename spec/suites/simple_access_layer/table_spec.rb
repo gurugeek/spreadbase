29450da2eaef410a751b3b8e3cdd994887bf992d
Saverio Miroddi <saverio.pub2@gmail.com>
2012-04-18 19:58:46 +0200

    Fixes to SDL::Table; filled remaining spec for it.

--- spec/suites/simple_access_layer/table_spec.rb
+++ spec/suites/simple_access_layer/table_spec.rb
@@ -35,14 +35,13 @@ describe SpreadBase::SimpleAccessLayer::Table do
     @style_1 = SAL::Style.new( 'saverio!', :test => true )
     @style_2 = SAL::Style.new( 'enrico!',  :test => true )
     @style_3 = SAL::Style.new( 'mixy!',    :test => true )
-    @style_4 = SAL::Style.new( 'KARAOKE',    :test => true )
+    @style_4 = SAL::Style.new( 'KARAOKE',  :test => true )
   end
 
   before :each do
     @table = SAL::Table.new(
       'largo al factotum!!',
       [           @style_1,               nil  ],
-
       [ @style_2, 1,                      nil  ],
       [ @style_3, [ Date.new, @style_4 ], 44.3 ],
     )
@@ -56,8 +55,6 @@ describe SpreadBase::SimpleAccessLayer::Table do
 
     @table.data.should == expected_data
   end
-
-  it "should initialize from a raw table"
 
   it "should access a cell (CASE 1: Fixnum column identifier)" do
     @table[ 0, 0 ].should == 1
@@ -106,9 +103,38 @@ describe SpreadBase::SimpleAccessLayer::Table do
     @table.row_style( 0 ).should == @style_3
   end
 
-  it "should insert a row (CASE 1: equal/less values than the columns number)"
+  it "should insert a row (CASE 1: equal/less values than the columns number)" do
+    @table.insert_row( 1, [ @style_4, 34, [ 'abc', @style_2 ] ] )
 
-  it "should insert a row (CASE 2: more values than the columns number)"
+    @table.data.size.should == 3
+
+    @table.row( 1 ).should == [ 34,       'abc' ]
+    @table.row( 2 ).should == [ Date.new, 44.3  ]
+
+    @table.row_style( 1 ).should == @style_4
+
+    @table.cell_style( 1, 0 ).should == nil
+    @table.cell_style( 1, 1 ).should == @style_2
+  end
+
+  it "should insert a row (CASE 2: more values than the columns number)" do
+    @table.insert_row( 4, [ @style_4, 34, [ 'abc', @style_2 ] ] )
+
+    @table.data.size.should == 5
+
+    @table.row( 2 ).should == [ nil, nil ]
+    @table.row( 3 ).should == [ nil, nil ]
+
+    @table.row( 4 ).should == [ 34, 'abc' ]
+  end
+
+  it "should insert a row (CASE 3: less values than the columns number)" do
+    @table.insert_row( 1, [ 34 ] )
+
+    @table.data.size.should == 3
+
+    @table.row( 1 ).should == [ 34, nil ]
+  end
 
   it "should access a column" do
     @table.column( 0 ).should == [ 1,   Date.new ]
@@ -123,9 +149,37 @@ describe SpreadBase::SimpleAccessLayer::Table do
     @table.column_style( 0 ). should == nil
   end
 
-  it "should insert a column (CASE 1: equal/less values than the rows number)"
+  it "should insert a column (CASE 1: equal/less values than the rows number)" do
+    @table.insert_column( 1, [ @style_4, 34, [ 'abc', @style_2 ] ] )
 
-  it "should insert a column (CASE 2: more values than the rows number)"
+    @table.data.size.should == 2
+
+    @table.row( 0 ).should == [ 1,        34,    nil  ]
+    @table.row( 1 ).should == [ Date.new, 'abc', 44.3 ]
+
+    @table.cell_style( 1, 1 ).should == @style_2
+  end
+
+  it "should insert a column (CASE 2: more values than the rows number)" do
+    @table.insert_column( 1, [ @style_4, 34, [ 'abc', @style_2 ], 1.1 ] )
+
+    @table.data.size.should == 3
+
+    @table.row( 0 ).should == [ 1,        34,    nil  ]
+    @table.row( 1 ).should == [ Date.new, 'abc', 44.3 ]
+    @table.row( 2 ).should == [ nil,      1.1,   nil  ]
+  end
+
+  it "should insert a column (CASE 3: less values than the rows number)" do
+    @table.insert_column( 1, [ @style_4, 34 ] )
+
+    @table.data.size.should == 2
+
+    @table.row( 0 ).should == [ 1,        34,  nil  ]
+    @table.row( 1 ).should == [ Date.new, nil, 44.3 ]
+
+    @table.cell_style( 1, 1 ).should == nil
+  end
 
   it "should return a column style" do
     @table.column_style( 0 ).should == @style_1
@@ -142,10 +196,28 @@ describe SpreadBase::SimpleAccessLayer::Table do
     @table.cell_style( 1, 1 ).should == nil
   end
 
-  it "should set a column style"
+  it "should set a column style" do
+    @table.set_column_style( 0, nil      )
+    @table.set_column_style( 1, @style_2 )
 
-  it "should set a row style"
+    @table.column_style( 0 ).should == nil
+    @table.column_style( 1 ).should == @style_2
+  end
 
-  it "should set a cell style"
+  it "should set a row style" do
+    @table.set_row_style( 0, @style_3 )
+    @table.set_row_style( 1, nil      )
+
+    @table.row_style( 0 ).should == @style_3
+    @table.row_style( 1 ).should == nil
+  end
+
+  it "should set a cell style" do
+    @table.set_cell_style( 0, 1, nil      )
+    @table.set_cell_style( 1, 1, @style_4 )
+
+    @table.cell_style( 0, 1 ).should == nil
+    @table.cell_style( 1, 1 ).should == @style_4
+  end
 
 end
